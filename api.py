73d0f44258d910036b39caf46beb17573e03458f
shivanisrivastava421-commits <shivanisrivastava421@gmail.com>
2026-04-23 09:38:23 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from fastapi import FastAPI
 from pydantic import BaseModel
 from services.analysis import generate_response
