c89ee56dce4c0321a1f77e0e7220f12f0f26e0a5
shivanisrivastava421-commits <shivanisrivastava421@gmail.com>
2026-04-23 08:04:41 +0530

    Update fairness.py

--- fairness.py
+++ fairness.py
@@ -1,4 +1,4 @@
-from fairlearn.metrics import MetricFrame, selection_rate
+'''from fairlearn.metrics import MetricFrame, selection_rate
 
 def compute_fairness(y, y_pred, sensitive_feature):
 
@@ -16,6 +16,24 @@ def compute_fairness(y, y_pred, sensitive_feature):
     eo = dp * 1.2
 
     # Disparate Impact
+    di = 1 - dp
+
+    return float(dp), float(eo), float(di)'''
+def compute_fairness(y, y_pred, sensitive_feature):
+
+    groups = set(sensitive_feature)
+    rates = []
+
+    for g in groups:
+        mask = (sensitive_feature == g)
+        total = mask.sum()
+        approved = y_pred[mask].sum()
+
+        rate = (approved / total) if total > 0 else 0
+        rates.append(rate)
+
+    dp = max(rates) - min(rates)
+    eo = dp
     di = 1 - dp
 
     return float(dp), float(eo), float(di)
